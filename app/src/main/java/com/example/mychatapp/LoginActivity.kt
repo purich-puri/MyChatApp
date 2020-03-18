540d8e0030d9e221542720324df40cf13f214290
purich <purich.puri@gmail.com>
2020-03-19 00:18:14 +1100

    Initial commit
    (cherry picked from commit 58af48f4dbd8d0dcf19620316c0242d76d9c7f87)
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/mychatapp/LoginActivity.kt
    #	app/src/main/java/com/example/mychatapp/MainActivity.kt
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml
    #	build.gradle

--- app/src/main/java/com/example/mychatapp/LoginActivity.kt
+++ app/src/main/java/com/example/mychatapp/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.example.mychatapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -13,11 +15,28 @@ class LoginActivity: AppCompatActivity() {
 
 
         loginButton_button_login.setOnClickListener {
-
+            performLogin()
         }
 
         registerButton_textView_login.setOnClickListener {
             finish()
         }
+    }
+
+    private fun performLogin(){
+        val email = login_editText_login.text.toString()
+        val password = password_editText_login.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){ task ->
+                Log.d("LoginActivity", "Logged in as: ${task.result?.user?.uid}")
+            }
+            .addOnFailureListener(this){ task ->
+                Log.d("Login Activity", "Failed to login: ${task.message}")
+            }
     }
 }
