f9234e36b72dd9e4c3a8825619c5555c02f86781
purich <purich.puri@gmail.com>
2020-03-19 02:59:14 +1100

    Firebase register function

--- app/src/main/java/com/example/mychatapp/LoginActivity.kt
+++ app/src/main/java/com/example/mychatapp/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mychatapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
@@ -9,5 +11,13 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
+        loginButton_button_login.setOnClickListener {
+
+        }
+
+        registerButton_textView_login.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/mychatapp/MainActivity.kt
+++ app/src/main/java/com/example/mychatapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +15,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         registerButton_button_register.setOnClickListener {
-            val email = email_editText_register.text.toString()
-            val password = password_editText_register.text.toString()
-
-            Log.d("MainActivity", "Email: $email")
-            Log.d("MainActivity", "Password: $password")
+            performRegister()
         }
 
         loginButton_textView_register.setOnClickListener {
             Log.d("MainActivity", "go to login page clicked")
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun performRegister(){
+        val email = email_editText_register.text.toString()
+        val password = password_editText_register.text.toString()
+        val passwordConfirm = passwordConfirm_editText_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()){
+            return
+        }
+        if (password == passwordConfirm){
+            if(password.length < 6){
+                //password length too short
+                Toast.makeText(this,"Password length too short", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            //register success
+                            Log.d("MainActivity","Success, User uid is: ${task.result?.user?.uid}"
+                            )
+
+                            Toast.makeText(this,"Registered Success!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            //if register failed
+                            Log.d("MainActivity", "Failed")
+                            Toast.makeText(this,"Failed to register!", Toast.LENGTH_SHORT).show()
+                            return@addOnCompleteListener
+                        }
+                    }
+                    .addOnFailureListener(this){ task ->
+                        Log.d("MainActivity", "Failed to create user: ${task.message}")
+                    }
+            }
+        }
+        else{
+            Log.d("MainActivity", "Confirm password does not match with password!")
+            Toast.makeText(this, "Password does not match!", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
